f338f55ddbe49c439fbaa0ec92fcb9be1d5e1430
Rohitkarmakargithub <rohitkarmakar2203@gmail.com>
2024-08-19 21:24:38 +0530

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,22 +2,24 @@ import { getToken } from "next-auth/jwt";
 import User from "@/models/User";
 import dbConnect from "@/utils/dbconnect";
 
-
 // check if user is logged in
 export default async function auth(req) {
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
-    if (!token) {
-        return null
-    }
     try {
-        dbConnect();
+        const token = await getToken({ req, secret: process.env.JWT_SECRET });
+        if (!token) {
+            console.error("Token not found or invalid.");
+            return null;
+        }
+
+        await dbConnect();  // Ensure db connection is established
         const user = await User.findOne({ email: token.email });
         if (!user) {
-            return null
+            return null;
         }
         return user;
+
     } catch (error) {
-        console.error(error);
-        return null
+        console.error("Error in authentication:", error);
+        return null;
     }
 }
