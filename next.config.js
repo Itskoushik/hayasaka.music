ea949b48c396f6d98836d1f7e7454e39f063c662
Koushik Ram <117310312+Itskoushik@users.noreply.github.com>
2026-05-11 16:36:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["c.saavncdn.com", "static.saavncdn.com", "www.jiosaavn.com"],
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
+    ],
   },
 };
 
